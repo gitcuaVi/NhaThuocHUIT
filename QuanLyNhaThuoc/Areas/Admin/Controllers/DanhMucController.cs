0a1cd9869ac7a45713f89b992860f87090712e6a
Trần Gia Vĩ <127858226+gitcuaVi@users.noreply.github.com>
2024-10-21 15:01:09 +0700

    feat: DanhMuc

--- QuanLyNhaThuoc/Areas/Admin/Controllers/DanhMucController.cs
+++ QuanLyNhaThuoc/Areas/Admin/Controllers/DanhMucController.cs
@@ -16,13 +16,36 @@ namespace QuanLyNhaThuoc.Areas.Admin.Controllers
             _context = context;
         }
 
-        [HttpGet]
+        // GET: /admin/danhmuc
+        [HttpGet("")]
         public IActionResult Index()
         {
             var danhMucList = _context.DanhMucs
-                              .Include(dm => dm.LoaiSanPhams) 
+                              .Include(dm => dm.LoaiSanPhams)
                               .ToList();
             return View(danhMucList);
         }
+
+        // GET: /admin/danhmuc/create
+        [HttpGet("create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /admin/danhmuc/create
+        [HttpPost("create")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(DanhMuc danhMuc)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(danhMuc);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(danhMuc);
+        }
     }
+
 }
