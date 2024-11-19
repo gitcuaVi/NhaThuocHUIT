6afaa1425898bce9bda094c429316fe750fd14af
Trần Gia Vĩ <127858226+gitcuaVi@users.noreply.github.com>
2024-11-19 09:11:20 +0700

    fix giao dien datahang

--- QuanLyNhaThuoc/Areas/KhachHang/Models/ThongTinDatHangViewModel.cs
+++ QuanLyNhaThuoc/Areas/KhachHang/Models/ThongTinDatHangViewModel.cs
@@ -8,6 +8,7 @@
         public int SoLuong { get; set; }
         public decimal DonGia { get; set; }
         public string TenSanPham { get; set; }
+        public string TrangThaiThanhToan { get; set; }
 
         // Thuộc tính Thành tiền
         public decimal ThanhTien => SoLuong * DonGia; // Tính Thành tiền
