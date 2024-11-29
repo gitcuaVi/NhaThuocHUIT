e1c20b54533d3affa6c25ecb744594a9579764c1
VietAnh <vanhdz246@gmail.com>
2024-11-29 23:56:44 +0700

    up2911

--- QuanLyNhaThuoc/Areas/KhachHang/Models/ProductViewDetailsModel.cs
+++ QuanLyNhaThuoc/Areas/KhachHang/Models/ProductViewDetailsModel.cs
@@ -2,15 +2,13 @@
 {
     public class ProductViewDetailsModel
     {
-
         public int MaThuoc { get; set; }
         public string TenThuoc { get; set; }
         public decimal DonGia { get; set; }
         public string DonVi { get; set; }
-        public string? HinhAnh { get; set; } 
-        
+        public string? HinhAnh { get; set; }
         public DateTime? HanSuDung { get; set; }
-        
+        public int? SoLuongTon { get; set; } 
+        public int MaDanhMuc { get; set; }
     }
 }
-
--- QuanLyNhaThuoc/Areas/KhachHang/Models/ProductViewModel.cs
+++ QuanLyNhaThuoc/Areas/KhachHang/Models/ProductViewModel.cs
@@ -6,6 +6,7 @@
         public string TenThuoc { get; set; }
         public decimal DonGia { get; set; }
         public string DonVi { get; set; }
-        public string HinhAnhDauTien { get; set; } 
+        public string HinhAnhDauTien { get; set; }
+        public int? SoLuongTon { get; set; }
     }
 }
