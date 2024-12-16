caf3e9275ec7ddc06f6d0ddb31c9ab0c56889dbb
Trần Gia Vĩ <127858226+gitcuaVi@users.noreply.github.com>
2024-12-16 12:04:30 +0700

    push

--- QuanLyNhaThuoc/Areas/KhachHang/Models/ProductViewDetailsModel.cs
+++ QuanLyNhaThuoc/Areas/KhachHang/Models/ProductViewDetailsModel.cs
@@ -3,6 +3,7 @@
     public class ProductViewDetailsModel
     {
         public int MaThuoc { get; set; }
+        public string TenDanhMuc { get; set; }
         public string TenThuoc { get; set; }
         public decimal DonGia { get; set; }
         public string DonVi { get; set; }
